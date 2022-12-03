34e05410dc3c205dabbb8bd35d3d3e2373b41f93
Aleksandr <mahnev.web@gmail.com>
2022-12-03 20:08:46 +0700

    script_update01

--- js/script.js
+++ js/script.js
@@ -3,8 +3,9 @@ let bg = document.querySelector('.hero');
 let expItems = document.querySelectorAll('.experience__item');
 let para = document.querySelector('.info-para');
 let ellipse = document.querySelector('.ellipse');
+let arrow = document.querySelector('.arrowDown');
 
-ellipse.style.top = '30px';
+ellipse.style.top = '0px';
 
 expItems.forEach((expItem) => {
     expItem.onclick = function() {
@@ -12,23 +13,27 @@ expItems.forEach((expItem) => {
         switch(expItemId) {
             case '1':
                 para.textContent = 'Первый вариант';
-                ellipse.style.top = '30px';
+                ellipse.style.top = '0px';
                 break;
             case '2':
                 para.textContent = 'Второй вариант';
-                ellipse.style.top = '114px';
+                ellipse.style.top = '85px';
                 break;
             case '3':
                 para.textContent = 'Третий вариант';
-                ellipse.style.top = '195px';
+                ellipse.style.top = '165px';
                 break;
             case '4':
                 para.textContent = 'Четвертый вариант';
-                ellipse.style.top = '280px';
+                ellipse.style.top = '250px';
                 break;
         }
     }
 })
 bttn.onclick = function () {
     bg.classList.toggle('blue');
+}
+
+arrow.onclick = function() {
+    
 }
